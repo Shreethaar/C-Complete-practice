e46b3147a9d1e66f55409f9430f666ac195aaa63
Shreethaar Arunagirinathan <64171155+Shreethaar@users.noreply.github.com>
2024-03-31 14:00:26 +0800

    Update 49.c

--- Moderate/49.c
+++ Moderate/49.c
@@ -3,15 +3,15 @@
 int main()
 {
 	int a,b;
-	int *ptr1,*ptr2,*temp;
+	int *ptr1,*ptr2,temp;
 	printf("Enter two numbers\n");
 	scanf("%d\t%d",&a,&b);
 	printf("The Value before swapping are %d and %d",a,b);
 	ptr1=&a;
 	ptr2=&b;
-	*temp=*ptr1;
+	temp=*ptr1;
 	*ptr1=*ptr2;
-	*ptr2=*temp;
+	*ptr2=temp;
 	printf("\nThe Value after swapping are %d and %d",a,b);
 	
 	
